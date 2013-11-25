da3706da0b0a993059ee95e2079ac51c6dd704c1
Pedro Dumont <pedrodm@outlook.com>
2013-11-25 11:45:48 -0300

    Main Activity Map Location Awareness
    Implementing location awareness on the main ativity map, using the
    LocationClient Api

--- src/com/dcc/matc89/spots/activity/MainActivity.java
+++ src/com/dcc/matc89/spots/activity/MainActivity.java
@@ -1,41 +1,111 @@
 package com.dcc.matc89.spots.activity;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.dcc.matc89.spots.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 // Para que a ActionBar funcione em todas as versões é necessário estender ActionBarActivity ao invés de Activity
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener {
+	
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+	private LocationClient mLocationClient;
+	private SupportMapFragment mapFragment;
+	private GoogleMap map;
+	private Location mCurrentLocation;
+	
+	
+	public static class ErrorDialogFragment extends DialogFragment{
+		private Dialog mDialog;
+		
+		public ErrorDialogFragment(){
+			super();
+			mDialog = null;
+		}
+		
+		public void setDialog(Dialog dialog) {
+			mDialog = dialog;
+		}
+		
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState){
+			return mDialog;
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		switch(requestCode){
+			case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+				switch(resultCode) {
+					case Activity.RESULT_OK :
+						
+					break;
+				}
+			}
+	}
+	
+	private boolean servicesConnected() {
+		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		ConnectionResult connectionResult = new ConnectionResult(resultCode, null);
+		if (resultCode == ConnectionResult.SUCCESS) {
+			Log.d("Location Updates", "Google Play services is available.");
+			return true;
+		} else {
+			int errorCode = connectionResult.getErrorCode();
+			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(errorCode, this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+			if (errorDialog != null) {
+				ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+				errorFragment.setDialog(errorDialog);
+				errorFragment.show(getSupportFragmentManager(), "Location Updates");
+			}
+		}
+		return false;
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-		GoogleMap map = mapFragment.getMap();
-
-		if(map != null){
-			map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-			LatLng myLocation = new LatLng(-12.97, -38.51);
-			map.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation,14));
-			
-			//TODO estudar LocationClient API e google play services http://developer.android.com/training/location/retrieve-current.html
-			//map.setMyLocationEnabled(true);
-			//LatLng myLocation = new LatLng(map.getMyLocation().getLatitude(), map.getMyLocation().getLongitude());
-			//map.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation,10));
+		mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+		map = mapFragment.getMap();
+		map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+		
+		if (servicesConnected()){
+			mLocationClient = new LocationClient(this,this,this);
 		}
+
 
 	}
 
+	@Override
+	protected void onStart(){
+		super.onStart();
+		mLocationClient.connect();
+	}
+
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -60,5 +130,42 @@ public class MainActivity extends ActionBarActivity {
 			break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		if (connectionResult.hasResolution()){
+			try {
+				connectionResult.startResolutionForResult(this,CONNECTION_FAILURE_RESOLUTION_REQUEST);
+			} catch (IntentSender.SendIntentException e) {
+				e.printStackTrace();
+			}
+		} else {
+			showErrorDialog(connectionResult.getErrorCode());
+		}
+	}
+
+	private void showErrorDialog(int errorCode) {
+		Toast.makeText(this, "Connection Error ".concat(String.valueOf(errorCode)), Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+		mCurrentLocation = mLocationClient.getLastLocation();
+		map.setMyLocationEnabled(true);
+		LatLng mLocationLatLng = new LatLng(mCurrentLocation.getLatitude(),mCurrentLocation.getLongitude());
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(mLocationLatLng, 15));
+	}
+
+	@Override
+	public void onDisconnected() {
+		Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	protected void onStop(){
+		mLocationClient.disconnect();
+		super.onStop();
 	}
 }
